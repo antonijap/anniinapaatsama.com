d5709313a17fdfaa083ac87b79cd86411700a0b0
Antonija <antonija.pek@nosto.com>
2019-11-25 15:07:08 +0200

    content applied

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,62 @@
 <template>
   <div class="wrapper">
     <prismic-rich-text v-if="home" :field="home.data.intro" class="intro"/>
-    <prismic-rich-text v-if="home" :field="home.data.contact"/>
-    <prismic-rich-text v-if="home" :field="home.data.about"/>
-    <prismic-rich-text v-if="home" :field="home.data.education"/>
-    <prismic-rich-text v-if="home" :field="home.data.experience"/>
-    <prismic-rich-text v-if="home" :field="home.data.extracurricular_activities"/>
-    <prismic-rich-text v-if="home" :field="home.data.fun_facts"/>
-    <prismic-rich-text v-if="home" :field="home.data.lets_connect"/>
+    <div class="container horizontal-stack-small">
+      <div>
+        <h3>Email</h3>
+        <prismic-rich-text v-if="home" :field="home.data.email"/>
+      </div>
+      <div>
+        <h3>Phone</h3>
+        <prismic-rich-text v-if="home" :field="home.data.phone"/>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="heading-with-line">
+        <h3>Projects</h3>
+        <div class="line"></div>
+      </div>
+      <p class="bottom-space">To be updated!</p>
+      <div class="line"></div>
+    </div>
+
+    <div class="container horizontal-stack top-space">
+      <div>
+        <div>
+          <h3>About</h3>
+          <prismic-rich-text v-if="home" :field="home.data.about"/>
+        </div>
+        <div class="container">
+          <h3>Extracurricular Activities</h3>
+          <prismic-rich-text v-if="home" :field="home.data.extracurricular_activities"/>
+        </div>
+      </div>
+
+      <div>
+        <div>
+          <h3>Education</h3>
+          <prismic-rich-text v-if="home" :field="home.data.education"/>
+        </div>
+        <div class="container">
+          <h3>Experience</h3>
+          <prismic-rich-text v-if="home" :field="home.data.experience"/>
+        </div>
+      </div>
+    </div>
+    
+    <div class="container">
+      <div>
+        <h3>Fun facts</h3>
+        <prismic-rich-text v-if="home" :field="home.data.fun_facts"/>
+      </div>
+
+      <div class="top-space">
+        <h3>Let's connect</h3>
+        <prismic-rich-text v-if="home" :field="home.data.lets_connect"/>
+      </div>
+    </div>
+    
     <!-- <div class="projects">
       <div class="project" v-for="(work, index) in works" :key="index">
         <span>{{ $prismic.richTextAsPlain(work.data.title) }}</span>
@@ -32,11 +81,77 @@
   }
 
   @include xl {
-    width: 60%;
+    width: 65%;
+  }
+
+  .intro {
+    margin-bottom: $space-x-large * 2;
   }
 
   /deep/ h1 {
-    font-size: 48px;
+    font-size: 43px;
+    line-height: 1.5;
+  }
+
+  /deep/ h3 {
+    font-size: 18px;
+    color: white;
+    font-weight: 600;
+    font-family: 'Hanken Grotesk SemiBold';
+    margin-bottom: $space-medium;
+    text-transform: uppercase;
+  }
+
+  /deep/ strong {
+    font-size: 18px;
+    font-weight: 600;
+    font-family: 'Hanken Grotesk SemiBold';
+  }
+
+  .container {
+    margin: $space-x-medium 0 0;
+
+    .heading-with-line {
+      display: grid;
+      grid-template-columns: auto 1fr;
+      grid-column-gap: 24px;
+    }
+
+    /deep/ p {
+      margin-bottom: $space-medium;
+      &:last-child {
+        margin-bottom: 0;
+      }
+    }
+  }
+
+  .horizontal-stack-small {
+    display: grid;
+    grid-template-columns: 30% 30%;
+  }
+
+  .horizontal-stack {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-column-gap: $space-x-large;
+  }
+
+  .line {
+    background: $color-white;
+    opacity: 0.2;
+    width: 100%;
+    display: block;
+    transform: translateY(16px);
+    height: 2px;
+    position: relative;
+  }
+
+  .top-space {
+    margin-top: $space-x-large;
+  }
+
+  .bottom-space {
+    margin-bottom: $space-medium;
   }
 
 }
